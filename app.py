85a9fff0c628f36700c72349396da1fc42aacd40
pranav <pranava.sri@gmail.com>
2023-05-03 10:31:05 +0530

    now the interface refreshes after submitting

--- app.py
+++ app.py
@@ -4,14 +4,12 @@ import gradio as gr
 
 def on_submit(img, category):
   print(category)
-  img.show()
+  return None, ''
 
 
 img = gr.Image(type='pil', image_mode='RGBA', label='Image')
 category = gr.Dropdown(choices=CATEGORIES.keys(), label='Category', multiselect=False, interactive=True)
-collimg = gr.Interface(
-  fn=on_submit, inputs=[img, category], outputs=None
-)
+collimg = gr.Interface(fn=on_submit, inputs=[img, category], outputs=[img, category], allow_flagging=False)
 
 
 collimg.launch(server_name=IP)
