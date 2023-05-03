8f5e61433045a726f969e237258e62d4e4f367ff
pranav <pranava.sri@gmail.com>
2023-05-03 11:38:59 +0530

    made category dropdown non-interactive, also now error is raised if any of image or category component is empty

--- app.py
+++ app.py
@@ -3,13 +3,16 @@ import gradio as gr
 
 
 def on_submit(img, category):
-  print(category)
+  if img is None:
+    raise gr.Error('Image is required')
+  if category=='':
+    raise gr.Error("Category shouldn't be empty")
   return None, ''
 
 
 img = gr.Image(type='pil', image_mode='RGBA', label='Image')
-category = gr.Dropdown(choices=CATEGORIES.keys(), label='Category', multiselect=False, interactive=True)
-collimg = gr.Interface(fn=on_submit, inputs=[img, category], outputs=[img, category], allow_flagging=False)
+category = gr.Dropdown(choices=CATEGORIES.keys(), label='Category', multiselect=False)
+collimg = gr.Interface(fn=on_submit, inputs=[img, category], outputs=[img, category], allow_flagging='never')
 
 
 collimg.launch(server_name=IP)
